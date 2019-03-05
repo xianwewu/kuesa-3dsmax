b3cb8bc4a14a7a88185a03b1801ba494ae15f278
Mike Krus <mike.krus@kdab.com>
2019-03-05 13:37:17 +0000

    Code sign the installer

--- deploy/build-installer.bat
+++ deploy/build-installer.bat
@@ -1,1 +1,3 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 ISCC.exe installer.iss
+signtool.exe sign  -t http://timestamp.globalsign.com/scripts/timestamp.dll  -fd SHA256 -v Output\InstallKuesaFor3DSMax.exe
